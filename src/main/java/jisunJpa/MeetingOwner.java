59c0f93a2277f43130a74ac6558febfd30ad63a3
DESKTOP-5N7EHG6\js <greybrown4645@gmail.com>
2024-08-07 15:05:44 +0900

    Update: JPA entity,repository 추가

---
+++ src/main/java/jisunJpa/MeetingOwner.java
@@ -0,0 +1,45 @@
+package jisunJpa;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "MEETING_T")
+public class MeetingOwner {
+  
+  //기본키
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "MEETING_NO", nullable = false)
+  private long meetingNo;
+  
+  //그 외(name 안쓰면 기본적으로는 카멜케이스 -> 스네이크케이스)
+  @Column(length = 30, nullable = false)
+  private String meetingTitle;
+  
+  @Column(nullable = false)
+  private long capacity;
+  
+  @Column(nullable = false)
+  private Timestamp createdDate;
+  
+  @Column(nullable = false)
+  private long cateNo;
+
+  @Column(length = 30, nullable = false)
+  private String tagNo;
+  
+  @Column(nullable = false)
+  private long memberNo;
+  
+}
---
+++ src/main/java/jisunJpa/MeetingOwnerRepository.java
@@ -0,0 +1,11 @@
+package jisunJpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MeetingOwnerRepository extends JpaRepository<MeetingOwner, Number>{
+
+  
+  
+  
+  
+}
